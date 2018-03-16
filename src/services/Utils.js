1106ce387cf94811c885903eda4f402fd7796a1b
Dave Balmain <dsbalmain@agiledigital.com.au>
2018-03-16 14:51:41 +1100

    Fix hard-coded slack clientId

--- src/services/Utils.js
+++ src/services/Utils.js
@@ -16,7 +16,7 @@ module.exports.installHTML = (clientId) =>
 		<body>
 			<h1>poet-bot</h1>
 			<p>Click the button to add @poet to Slack!</p>
-			<a href="https://slack.com/oauth/authorize?client_id=284688040465.284688468193&scope=channels:history,bot,users.profile:read,users:read">
+			<a href="https://slack.com/oauth/authorize?client_id=${clientId}&scope=channels:history,bot,users.profile:read,users:read">
 			    <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
 			</a>
 		</body>
